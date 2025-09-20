38f882973066ad165d6a63775a69b5ed4b6a92b8
S Ctln <165759233+itnasctln@users.noreply.github.com>
2025-09-20 19:03:08 -0300

    "agregamos el switch para poder leer o escribir los arrays, proximamente me gustaria agregar algun if que verifique al leer si el indice esta en 0 y responder automaticamente que no esta cargado y si desea cargarlo o algo por el estilo."

--- Program.cs
+++ Program.cs
@@ -7,68 +7,126 @@ namespace revemosarray
     {
         static void Main(string[] args)
         {
+
+            //ACTUALIZACION 2: vamos a hacer un switch para elegir si queremos leer o cargar informacion.
+
+            //int leerOCargar = 0;
+
+            Console.WriteLine("Desea cargar o leer informacion?");
+            Console.WriteLine("ingrese a para leer");
+            Console.WriteLine("ingrese b para cargar informacion");
+            string leerOCargar = Console.ReadLine();
+            
             //vamos a crear 3 arrays de 31 espacios, para guardar la informacion
             //de las temperaturas de un mes, registradas a la mañana, tarde y noche.
 
             double[] temperaturaMañana = new double[31];
             double[] temperaturaTarde = new double[31];
             double[] temperaturaNoche = new double[31];
-
-
-            //vamos a hacer una carga en un dia especifico, primero consultamos un
-            //a que vector le agregaremos la informacion (mañana,tarde,noche),
-            //luego consultamos en que dia (indice), y por ultimo la temperatura.
-
-            Console.WriteLine("Iniciamos el programa para cargar temperaturas");
-
-            //variable para el switch
-            int selectorSwitch = 0;
             int diaSeleccionado = 0;
-            double temperaturaIngresada = 0.0;
-
-            Console.WriteLine("para cargar temperaturas de la mañana, ingrese 1");
-            Console.WriteLine("para cargar temperaturas de la tarde, ingrese 2");
-            Console.WriteLine("para cargar temperaturas de la noche, ingrese 3");
-
-            selectorSwitch = int.Parse(Console.ReadLine());
 
             
-            switch (selectorSwitch)
+
+            switch (leerOCargar)
             {
-                case 1:
-                   // Código si variable == TEMPERATURA MAÑANA
-                    Console.WriteLine("que día quiere cargar?(1-31)");
-                    diaSeleccionado = int.Parse(Console.ReadLine());
-                    Console.WriteLine("ingrese la temperatura");
-                    temperaturaIngresada = double.Parse(Console.ReadLine());
-                    temperaturaMañana[diaSeleccionado - 1] = temperaturaIngresada;
-                    Console.WriteLine("ustede cargo la temperatura " + temperaturaMañana[diaSeleccionado - 1] + " en el dia " + diaSeleccionado);
-                    break;
+                case "a":
+                    //en este caso vamos a leer informacion de los arrays, vamos a solicitar 
+                    //si quiere leer informacion de la mañana, tarde o noche y luego el dia.
 
-                case 2:
-                    // Código si variable == valor2 TEMPERATURA TARDE
-                    Console.WriteLine("que día quiere cargar?(1-31)");
-                    diaSeleccionado = int.Parse(Console.ReadLine());
-                    Console.WriteLine("ingrese la temperatura");
-                    temperaturaIngresada = double.Parse(Console.ReadLine());
-                    temperaturaTarde[diaSeleccionado - 1] = temperaturaIngresada;
-                    Console.WriteLine("ustede cargo la temperatura " + temperaturaTarde[diaSeleccionado - 1] + " en el dia " + diaSeleccionado);
-                    break;
+                    Console.WriteLine("Selecciono leer informacion");
+                    Console.WriteLine("de que turno desea leer la informacion?");
+                    Console.WriteLine("ingrese M para la Mañana");
+                    Console.WriteLine("ingrese T para la Tarde");
+                    Console.WriteLine("ingrese N para la Noche");
 
-                case 3:
-                    // Código si variable == valor3 TEMPERATURA NOCHE
-                    Console.WriteLine("que día quiere cargar?(1-31)");
+                    string selectorTurno = Console.ReadLine();
+                    Console.WriteLine("que dia quiere leer?");
                     diaSeleccionado = int.Parse(Console.ReadLine());
-                    Console.WriteLine("ingrese la temperatura");
-                    temperaturaIngresada = double.Parse(Console.ReadLine());
-                    temperaturaNoche[diaSeleccionado - 1] = temperaturaIngresada;
-                    Console.WriteLine("ustede cargo la temperatura " + temperaturaNoche[diaSeleccionado - 1] + " en el dia " + diaSeleccionado);
-                    break;
 
+                    switch (selectorTurno)
+                    {
+
+                        case "M":                        
+                            Console.WriteLine("la temperatura de la Mañana del dia" + diaSeleccionado + " es " + temperaturaMañana[diaSeleccionado]);
+                            break;
+
+                        case "T":
+                            Console.WriteLine("la temperatura de la Tarde del dia" + diaSeleccionado + " es " + temperaturaTarde[diaSeleccionado]);
+                            break;
+
+                        case "N":
+                            Console.WriteLine("la temperatura de la Noche del dia" + diaSeleccionado + " es " + temperaturaNoche[diaSeleccionado]);
+                            break;
+
+                        default:
+                            Console.WriteLine("no es una opcion valida, volviendo al menu");
+                        break;
+                    }   
+                        break;
+                case "b":
+                    
+                    //vamos a hacer una carga en un dia especifico, primero consultamos
+                    //a que vector le agregaremos la informacion (mañana,tarde,noche),
+                    //luego consultamos en que dia (indice), y por ultimo la temperatura
+
+                    Console.WriteLine("Iniciamos el programa para cargar temperaturas");
+
+                    //variable para el switch
+                    int selectorSwitch = 0;
+                    
+                    double temperaturaIngresada = 0.0;
+
+                    Console.WriteLine("para cargar temperaturas de la mañana, ingrese 1");
+                    Console.WriteLine("para cargar temperaturas de la tarde, ingrese 2");
+                    Console.WriteLine("para cargar temperaturas de la noche, ingrese 3");
+
+                    selectorSwitch = int.Parse(Console.ReadLine());
+                    
+                
+
+                    switch (selectorSwitch)
+                    {
+                        case 1:
+                            // Código si variable == TEMPERATURA MAÑANA
+                            Console.WriteLine("que día quiere cargar?(1-31)");
+                            diaSeleccionado = int.Parse(Console.ReadLine());
+                            Console.WriteLine("ingrese la temperatura");
+                            temperaturaIngresada = double.Parse(Console.ReadLine());
+                            temperaturaMañana[diaSeleccionado - 1] = temperaturaIngresada;
+                            Console.WriteLine("ustede cargo la temperatura " + temperaturaMañana[diaSeleccionado - 1] + " en el día " + diaSeleccionado + " a la mañana");
+                            break;
+
+                        case 2:
+                            // Código si variable == valor2 TEMPERATURA TARDE
+                            Console.WriteLine("que día quiere cargar?(1-31)");
+                            diaSeleccionado = int.Parse(Console.ReadLine());
+                            Console.WriteLine("ingrese la temperatura");
+                            temperaturaIngresada = double.Parse(Console.ReadLine());
+                            temperaturaTarde[diaSeleccionado - 1] = temperaturaIngresada;
+                            Console.WriteLine("ustede cargo la temperatura " + temperaturaTarde[diaSeleccionado - 1] + " en el día " + diaSeleccionado + " a la tarde");
+                            break;
+
+                        case 3:
+                            // Código si variable == valor3 TEMPERATURA NOCHE
+                            Console.WriteLine("que día quiere cargar?(1-31)");
+                            diaSeleccionado = int.Parse(Console.ReadLine());
+                            Console.WriteLine("ingrese la temperatura");
+                            temperaturaIngresada = double.Parse(Console.ReadLine());
+                            temperaturaNoche[diaSeleccionado - 1] = temperaturaIngresada;
+                            Console.WriteLine("ustede cargo la temperatura " + temperaturaNoche[diaSeleccionado - 1] + " en el día " + diaSeleccionado + " a la noche");
+                            break;
+
+                        default:
+                            Console.WriteLine("no es una opcion valida, saliendo del programa");
+                            break;
+                    }
+                    break;
                 default:
                     Console.WriteLine("no es una opcion valida, saliendo del programa");
                     break;
+
             }
+                       
 
         }
 
